e7a4b0e8eca60cf5ec845604bb2ad6e28a1d5089
Kalyan9493 <57888686+Kalyan9493@users.noreply.github.com>
2022-11-17 12:14:15 +0530

    Update step_script.sh

--- bin/step_script.sh
+++ bin/step_script.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# example of using arguments to a script
+echo "Internal Script execution started"
 echo "Environment is $1"
 echo "Tenant is $2"
 echo "Step Id is $3"
 echo "Script Executed for $1 environment and $2 Tenant and $3 Step"
+echo "Internal Script Execution completed"
 exit -1
